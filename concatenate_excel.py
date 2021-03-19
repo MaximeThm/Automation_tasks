d863b367ceb85eeaf771c1bcbc3d4e4e0dd33eb0
Maxime <35689674+MaximeThm@users.noreply.github.com>
2021-03-19 23:14:07 +0100

    Update concatenate_excel.py

--- concatenate_excel.py
+++ concatenate_excel.py
@@ -1,15 +1,18 @@
 import os
 import pandas as pd
+from tkinter.filedialog import askdirectory
 
-root = '/Users/maximethomas/Desktop/CORSAIR/From_Client/Excel-new'
 df = pd.DataFrame()
+
+#ask user to select directory
+root = askdirectory()
 files = os.listdir(root)
 
-
+#concatenate all excel files in one
 for file in files:
     file = os.path.join(root, file)
     if file.endswith('.xlsx'):
         df = df.append(pd.read_excel(file), ignore_index=True)
 
 print(df)
-#df.to_excel('output.xlsx')
+df.to_excel('output.xlsx')
